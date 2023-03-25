f434a9b9acdfa86fcb4a35790b3a9dbb951d6174
Alexandra <alexandravolchihina@yandex.ru>
2023-03-25 23:45:33 +0300

    hw

--- src/test/java/com/skypro/testing/shopping/ShoppingCartTest.java
+++ src/test/java/com/skypro/testing/shopping/ShoppingCartTest.java
@@ -28,12 +28,7 @@ public class ShoppingCartTest {
     private ShoppingCart shoppingCart;
     private UUID userId;
     private Map<UUID, List<Item>> users;
-    private Item item1;
-    private Item item2;
-    private Item item3;
-    private Item item4;
-    private Item item5;
-    private Item item6;
+    private Item item;
     @Spy
     List<Item> items = new ArrayList<>();
 
@@ -42,6 +37,14 @@ public class ShoppingCartTest {
     public void setUp() {
         //создаем тестовые данные
         userId = UUID.randomUUID();
+        /* Почему, когда так прописываю, а затем вызываю item3, выдает ошибку?
+        Mockito.when(item1).thenReturn(new Item("Евгений", 30));
+        Mockito.when(item2).thenReturn(new Item("Марина", 60));
+        Mockito.when(item3).thenReturn(new Item("Алина", 90));
+        Mockito.when(item4).thenReturn(new Item("Ветерок", 100));
+        Mockito.when(item5).thenReturn(new Item("Ежик", 0));
+        Mockito.when(item6).thenReturn(new Item("", 10));
+         */
         Item item1 = new Item("Евгений", 30);
         Item item2 = new Item("Марина", 60);
         Item item3 = new Item("Алина", 90);
@@ -52,47 +55,46 @@ public class ShoppingCartTest {
         users.put(userId, new ArrayList<>());
 
     }
-    private static Stream<Arguments> provideObjectsForCheckNull() {
-
-        Stream<Arguments> of = Stream.of(
-                Arguments.of(null, new Item(" ", 100)),
-                Arguments.of(UUID.randomUUID(), null),
-                Arguments.of(null, null)
-        );
-        return of;
-    }
     @Test
     public void testAddToCartItem() {
-       doNothing().when(dao).addItem(userId,item1);
+        Item item1 = new Item("Евгений", 30);
+        doNothing().when(dao).addItem(userId,item1);
         shoppingCart.addToCart(userId, item1);
         verify(dao, only()).addItem(userId,item1);
     }
     @Test
     public void testAddToCartWithEmptyNameOfItem() {
-        Mockito.when(item5.getName().isBlank()).thenThrow(CartValidationException.class);
-        Assertions.assertThrows(CartValidationException.class, () -> item5.getName().isBlank());
+        Item item6 = new Item("", 10);
+//        items.add(item6);
+        when(item6.getName().isEmpty()).thenThrow(CartValidationException.class);
+        Assertions.assertThrows(CartValidationException.class, () -> item6.getName().isEmpty());
     }
-    @ParameterizedTest
-    @MethodSource("provideObjectsForCheckNull")
+    @Test
     public void testRemoveFromCartWhenItemsHasEmptyField() {
         Assertions.assertThrows(CartValidationException.class, () -> shoppingCart.removeFromCart(userId, new Item("", 100)));
     }
-    @ParameterizedTest
-    @MethodSource("provideObjectsForCheckNull")
+    @Test
+
     public void testRemoveFromCartWhenItemsHasNullUserId() {
+        Item item1 = new Item("Евгений", 30);
         Assertions.assertThrows(CartValidationException.class, () -> shoppingCart.addToCart(null, item1));
     }
     @Test
     public void removeFromCart2() {
+        Item item2 = new Item("Марина", 60);
         when(dao.removeItem(userId, item2)).thenReturn(true);
         shoppingCart.removeFromCart(userId, item2);
         verify(dao, only()).removeItem(userId, item2);
     }
     @Test
     public void testCalculateCartItemsCountForUsersFromList() {
+        Item item1 = new Item("Евгений", 30);
+        Item item2 = new Item("Марина", 60);
+        Item item3 = new Item("Алина", 90);
+        Item item4 = new Item("Ветерок", 100);
         items.add(item1);
         items.add(item3);
-        items.add(item1);
+        items.add(item2);
         items.add(item4);
         Mockito.when(dao.getItems(userId)).thenReturn(items);
         shoppingCart.calculateCartItemsCount(userId);
@@ -100,6 +102,7 @@ public class ShoppingCartTest {
     }
     @Test
     public void testCalculateCartItemsCountForUsersFromList2() {
+        Item item4 = new Item("Ветерок", 100);
         items.add(item4);
         when(dao.getItems(userId)).thenReturn(items);
         int size = shoppingCart.calculateCartItemsCount(userId);
@@ -111,13 +114,15 @@ public class ShoppingCartTest {
         shoppingCart.calculateCartItemsCount(null);
         Assertions.assertEquals(0, shoppingCart.calculateCartItemsCount(null));
     }
-@Test
+    @Test
     public void calculateTotalInCartForUsersFromList() {
+        Item item1 = new Item("Евгений", 30);
+        Item item2 = new Item("Марина", 60);
         items.add(item1);
         items. add(item2);
         Mockito.when(dao.getItems(userId)).thenReturn(items);
         shoppingCart.calculateTotalInCart(userId);
-        Assertions.assertEquals(2, shoppingCart.calculateTotalInCart(userId));    }
+        Assertions.assertEquals(90, shoppingCart.calculateTotalInCart(userId));    }
     @Test
     public void calculateTotalInCartWhenItemsHasNullUserId() {
         Mockito.when(dao.getItems(null)).thenReturn(items);
